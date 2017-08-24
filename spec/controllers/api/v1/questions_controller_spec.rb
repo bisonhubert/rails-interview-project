ce21de83909d5e8117ae87458337f405e74eb6f0
Bison Hubert <davidbisonhubert@gmail.com>
2017-08-23 17:20:23 -0700

    Add success test and tenant factory

--- spec/controllers/api/v1/questions_controller_spec.rb
+++ spec/controllers/api/v1/questions_controller_spec.rb
@@ -1,4 +1,16 @@
 describe Api::V1::QuestionsController do
+  context 'valid token' do
+    let(:tenant) { FactoryGirl.create(:tenant) }
+    
+    describe 'GET #index' do
+      it 'returns a success 200 response' do
+        request.headers['token'] = tenant.api_key
+        get :index, :format => :json
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+
   context 'invalid token' do
     describe 'GET #index' do
       it 'returns an unauthorized 401 response' do
